e7cb8db7c2320adf1ae41104faf09894b9e720e1
Thanushkanth Shanmuganathan <35372972+Dev-efil@users.noreply.github.com>
2021-12-09 10:29:59 +0530

    Socket setup on client

--- Client-Socket/src/app/app.component.ts
+++ Client-Socket/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SocketioService } from './service/socketio.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client-Socket';
+
+  constructor(private ioConnect: SocketioService) {}
+
+  ngOnInit() {
+    this.ioConnect.socketConnection();
+  }
 }
