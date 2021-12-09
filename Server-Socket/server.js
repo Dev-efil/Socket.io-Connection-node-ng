50728032911d83d164c9a030f57bd528c5250a5d
Thanushkanth Shanmuganathan <35372972+Dev-efil@users.noreply.github.com>
2021-12-09 10:30:56 +0530

    enable cors in server

--- Server-Socket/server.js
+++ Server-Socket/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const socket = require('socket.io');
 
 // Server setup
 const server = express();
-const PORT = process.env.PORT || 3000;
 
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+
+
+const PORT = process.env.PORT || 3000;
 const app = server.listen(PORT, () => console.log(`Server started on port : ${PORT}`));
 
 // Socket.io setup
-const io = socket(app);
+const io = socket(app, {
+    cors: {
+      origin: "*"
+    }
+});
 
-io.on('connection', function(socket) {
-    console.log('Made a Socket connection');
+io.on('connection', function (socket) {
+    console.log(`Made a Socket connection : ${socket.id}`);
 });
